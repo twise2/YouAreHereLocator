b4bc8915d021fd3ac1f8e857fd2bc004f78eb6ef
nicholasfay <fayn@rpi.edu>
2016-12-04 22:42:19 -0500

    Works better

--- FrontEnd/js/scripts.js
+++ FrontEnd/js/scripts.js
@@ -1,10 +1,45 @@
-// Empty JS for your own code to be here
-// 
-$(document).ready(function (){
-	$('#RPI').click(function(e){
-		console.log(e)
+var coords = []
+
+$(document).ready(function() {
+	//Collects and stores image click
+	$('#RPI').click(function(e) {
 		var offset = $(this).offset();
-    alert(e.pageX - offset.left);
-    alert(e.pageY - offset.top);
+		coords[0] = e.pageX - offset.left
+		coords[1] = e.pageY - offset.top
 	})
+
+	// Allows for error checking before submission
+	$("#adminForm").submit(function(event) {
+		event.preventDefault();
+
+		var $form = $(this)
+		var	name_value = $form.find('input[id="training"]').val()
+		var formData = new FormData($("#adminForm")[0]);
+		if (name_value){
+			name_value = name_value.split('\\').pop().split('.')[0]
+		}
+		if (!name_value){
+			alert('Please upload a file before hitting submit')
+		} 
+		else{
+			url = $form.attr('action');
+		}
+		if (coords.length == 0) {
+			alert('Please click the map position before submitting files')
+		}
+		else {
+			var posting = $.ajax({
+				url: url,
+				type: 'POST',
+				name: name_value,
+				data: formData,
+				processData: false,
+				contentType: false
+			});
+
+			posting.done(function(data) {
+				console.log(data)
+			});
+		}
+ })
 })
